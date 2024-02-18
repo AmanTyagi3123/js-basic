1fd59f7b619a6e905af7ca978f8a18ba8e5b2503
Aman Tyagi <129617905+AmanTyagi3123@users.noreply.github.com>
2024-02-18 18:52:42 +0000

    Slight information of data types in JS

---
+++ .devcontainer/01_basics/02_data_types.js
@@ -0,0 +1,32 @@
+"use strict";   // treat all JS code as newer version.
+
+// alert( 3 + 3 ) --> We are using node js, not browser since we don't have a html code or are not writing in browser console.
+
+// Always keep code readability high.
+
+// ECMAScript provides the standards of JS.
+
+let name = "Aman Tyagi" // This is string data type.
+let age = 18  // This is a number data type.
+let isLoggedIn = true // This is boolean data type.
+
+/* 
+                                            DATA TYPES
+                                            **********
+
+                                            1. number
+                                            2. bigint
+                                            3. string
+                                            4. null => Standalone value along with a data type which represents empty.
+                                            5. undefined
+                                            6. symbol => unique
+
+    **Important
+    ------------
+    |***************************************************|
+    | Null is an object while undefined is a data type. |
+    |___________________________________________________|
+*/
+
+console.log(typeof undefined);  // Undefined.
+console.log(typeof null);   // Object.
