79eb0c421b61bcac8dbd6079ca5f79e7cfabce73
Aman Tyagi <129617905+AmanTyagi3123@users.noreply.github.com>
2024-02-20 17:21:04 +0000

    Data types Summary

---
+++ .devcontainer/01_basics/dataTypes_summary.js
@@ -0,0 +1,15 @@
+// Primitive
+
+// 7 Types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Javascript is dynamically typed.
+
+// Reference or Non-Primitive
+
+// Array, Objects, Functions
+
+const myfunc = function(){
+    console.log("Hello World!");
+}
+
+console.log(typeof myfunc);
