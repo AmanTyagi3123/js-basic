f3695fc50295183e2423ae0788b50463b8bd6516
Aman Tyagi <129617905+AmanTyagi3123@users.noreply.github.com>
2024-02-21 20:51:14 +0000

    Adding new information gathered on objects.

---
+++ .devcontainer/02_basics/03_objects.js
@@ -0,0 +1,40 @@
+// Singleton -> Whenever you create using constructor singleton is obtained. If literal is used o declare object then no 
+// no singleton formed.
+
+// Object literals
+
+const mysym = Symbol("key1")
+
+const JSuser = {
+    name : "Aman",
+    "full name" : "Aman Tyagi",
+    [mysym] : "mykey1", // Important :- Way of including symbol in an object.
+    age : 20,
+    location : "Noida",
+    email : "abc@gmail.com",
+    isLoggedIn : false,
+    lastLoginDays : ["Monday", "Saturday"] 
+}
+
+console.log(JSuser.email)
+console.log(JSuser["email"]) // These are the two ways of accessing any data member of an object. Prefer second.
+console.log(JSuser["full name"])
+console.log(JSuser[mysym])
+
+// Changing any value of an object ->
+JSuser.email = "bcd@gmail.com"
+// Object.freeze(JSuser) -> After this you cannot overwrite the value of JSuser object.
+JSuser.email = "abc@gmail.com"
+
+
+// Defining function in JS
+JSuser.greeting = function(){
+    console.log("Hello JS user")
+}
+
+JSuser.greeting2 = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JSuser.greeting())
+console.log(JSuser.greeting2())
