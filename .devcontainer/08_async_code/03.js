e7349f46ebd1c5394c1a9fe3747df966f1056c85
Aman Tyagi <129617905+AmanTyagi3123@users.noreply.github.com>
2024-03-07 09:52:28 +0000

    Background Color random changing

---
+++ .devcontainer/08_async_code/03.js
@@ -0,0 +1,26 @@
+// Creating function for selecting random color -->
+
+function randomColor(){
+    const str = "0123456789ABCDEF";
+    let random = '#';
+
+    for(let i=0; i<6; i++){
+        random += str[Math.floor(Math.random() * 16)];
+    }
+    return random;
+}
+
+const body = document.querySelector('body');
+let changeIt;
+document.querySelector('#start')
+.addEventListener('click',function(){
+    changeIt = setInterval(function(){
+        body.style.backgroundColor = randomColor()
+    },1000);
+})
+
+document.querySelector('#stop')
+.addEventListener('click',function(){
+    clearInterval(changeIt);
+    changeIt = NULL;
+})
